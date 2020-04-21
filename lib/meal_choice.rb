8e762e6d58f2a6bbb61b8048bce5e1b191f617e3
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-21 09:29:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,5 +7,5 @@ def meal_choice (veg1, veg2, protein = 'tofu')
   meal
 end
 meal_choice("carrots", "string beans", "chicken")
-meal_choice ("broccoli", "macaroni")
+meal_choice("broccoli", "macaroni")
 meal_choice ("broccoli", "macaroni", "roast beef")
